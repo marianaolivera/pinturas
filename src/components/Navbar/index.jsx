5060a765fd49163f952ed3ed4d0e5ad0395c1d1c
Mariana <marianaolivera83@gmail.com>
2022-10-20 20:33:14 -0300

    estilosnavbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,6 @@
 const Navbar = () => {
     return(
         <div>
-        <li class="nav-item">Inicio</li>
         <li class="nav-item">Productos</li>
         <li class="nav-item">Carrito</li>
         <li class="nav-item">Contacto</li>
