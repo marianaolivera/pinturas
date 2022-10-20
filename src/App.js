af8ae1a3f421722ec2490d738c90c1fc6f7d3a62
Mariana <marianaolivera83@gmail.com>
2022-10-19 21:33:53 -0300

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img width="250" height="57" src="https://pintarconacuarelas.com/wp-content/uploads/2021/03/cropped-logo-pintar-con-acuarelas-gris.png" class="custom-logo entered lazyloaded" alt="Pintar Con Acuarelas" data-lazy-src="https://pintarconacuarelas.com/wp-content/uploads/2021/03/cropped-logo-pintar-con-acuarelas-gris.png" data-ll-status="loaded"></img>
+      <div className='header__nav'><h3>Productos</h3><h3>Contacto</h3><h3>Carrito</h3></div>
+     </header>
+      <main className='products'>
+        <div className='card'>
+           <span className='card__name'>Cachorra</span>
+           <p className='card__description'>Cuadros pintados a mano en acuarelas, medida 20 x 28cm. Enmarcado en kiri listo para colgar.
+
+Lo que se entrega es el original pintado a mano.
+
+Podés elegir los colores y estilo de flores que más te gusten. 
+
+Una vez realizada la compra te contactás por medio de nuestro mail (mariana83@gmail.com) y definimos bien el estilo del cuadro. También podés contactarte por este medio para ver otras medidas. </p>
+           <span className='card__price'>$2500</span>
+           <span className='card__stock'>5 Unidades</span>
+        </div>
+        <div className='card'>
+           <span className='card__name'>Cachorra</span>
+           <p className='card__description'>Cuadros pintados a mano en acuarelas, medida 20 x 28cm. Enmarcado en kiri listo para colgar.
+
+Lo que se entrega es el original pintado a mano.
+
+Podés elegir los colores y estilo de flores que más te gusten. 
+
+Una vez realizada la compra te contactás por medio de nuestro mail (mariana83@gmail.com) y definimos bien el estilo del cuadro. También podés contactarte por este medio para ver otras medidas. </p>
+           <span className='card__price'>$2500</span>
+           <span className='card__stock'>5 Unidades</span>
+        </div>
+        <div className='card'>
+           <span className='card__name'>Cachorra</span>
+           <p className='card__description'>Cuadros pintados a mano en acuarelas, medida 20 x 28cm. Enmarcado en kiri listo para colgar.
+
+Lo que se entrega es el original pintado a mano.
+
+Podés elegir los colores y estilo de flores que más te gusten. 
+
+Una vez realizada la compra te contactás por medio de nuestro mail (mariana83@gmail.com) y definimos bien el estilo del cuadro. También podés contactarte por este medio para ver otras medidas. </p>
+           <span className='card__price'>$2500</span>
+           <span className='card__stock'>5 Unidades</span>
+        </div>
+        <div className='card'>
+           <span className='card__name'>Cachorra</span>
+           <p className='card__description'>Cuadros pintados a mano en acuarelas, medida 20 x 28cm. Enmarcado en kiri listo para colgar.
+
+Lo que se entrega es el original pintado a mano.
+
+Podés elegir los colores y estilo de flores que más te gusten. 
+
+Una vez realizada la compra te contactás por medio de nuestro mail (mariana83@gmail.com) y definimos bien el estilo del cuadro. También podés contactarte por este medio para ver otras medidas. </p>
+           <span className='card__price'>$2500</span>
+           <span className='card__stock'>5 Unidades</span>
+        </div>
+      
+      </main>
+      <footer className='footer'>
+        <h3 className='footer__item'>Politica de Privacidad</h3>
+
+      </footer>
     </div>
   );
 }
