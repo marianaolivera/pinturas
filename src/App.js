8c3f7fef17ac437a07bf23384c36d905accde5c6
Mariana <marianaolivera83@gmail.com>
2022-10-20 18:58:07 -0300

    card

--- src/App.js
+++ src/App.js
@@ -10,7 +10,21 @@ function App() {
      </header>
       <main className='products'>
         <div className='card'>
-           <span className='card__name'>Cachorra</span>
+           <img src="https://assets.bigcartel.com/product_images/322761267/size_real_tower.jpg?auto=format&fit=max&w=650" alt="Londres" />
+           <span className='card__name'>Londres</span>
+           <p className='card__description'>Cuadros pintados a mano en acuarelas, medida 20 x 28cm. Enmarcado en kiri listo para colgar.
+
+Lo que se entrega es el original pintado a mano.
+
+Podés elegir los colores y estilo de flores que más te gusten. 
+
+Una vez realizada la compra te contactás por medio de nuestro mail (mariana83@gmail.com) y definimos bien el estilo del cuadro. También podés contactarte por este medio para ver otras medidas. </p>
+           <span className='card__price'>$2500</span>
+           <span className='card__stock'>5 Unidades</span>
+        </div>
+ <div className='card'>
+          <img src="https://assets.bigcartel.com/product_images/322761624/size_real_lisboa.jpg?auto=format&fit=max&w=650" alt="" />
+           <span className='card__name'>Lisboa</span>
            <p className='card__description'>Cuadros pintados a mano en acuarelas, medida 20 x 28cm. Enmarcado en kiri listo para colgar.
 
 Lo que se entrega es el original pintado a mano.
@@ -22,7 +36,8 @@ Una vez realizada la compra te contactás por medio de nuestro mail (mariana83@g
            <span className='card__stock'>5 Unidades</span>
         </div>
         <div className='card'>
-           <span className='card__name'>Cachorra</span>
+          <img src="https://assets.bigcartel.com/product_images/335288715/size_real_flatiron.jpg?auto=format&fit=max&w=650" alt="" />
+           <span className='card__name'>New York</span>
            <p className='card__description'>Cuadros pintados a mano en acuarelas, medida 20 x 28cm. Enmarcado en kiri listo para colgar.
 
 Lo que se entrega es el original pintado a mano.
@@ -34,19 +49,8 @@ Una vez realizada la compra te contactás por medio de nuestro mail (mariana83@g
            <span className='card__stock'>5 Unidades</span>
         </div>
         <div className='card'>
-           <span className='card__name'>Cachorra</span>
-           <p className='card__description'>Cuadros pintados a mano en acuarelas, medida 20 x 28cm. Enmarcado en kiri listo para colgar.
-
-Lo que se entrega es el original pintado a mano.
-
-Podés elegir los colores y estilo de flores que más te gusten. 
-
-Una vez realizada la compra te contactás por medio de nuestro mail (mariana83@gmail.com) y definimos bien el estilo del cuadro. También podés contactarte por este medio para ver otras medidas. </p>
-           <span className='card__price'>$2500</span>
-           <span className='card__stock'>5 Unidades</span>
-        </div>
-        <div className='card'>
-           <span className='card__name'>Cachorra</span>
+          <img src="https://assets.bigcartel.com/product_images/316892313/ibiza_real.jpg?auto=format&fit=max&w=650" alt="" />
+           <span className='card__name'>Ibiza</span>
            <p className='card__description'>Cuadros pintados a mano en acuarelas, medida 20 x 28cm. Enmarcado en kiri listo para colgar.
 
 Lo que se entrega es el original pintado a mano.
